31f28bd85783dcb7d694ebfb3479d985d4bc1359
Martín Vega <maria.dolores.martin@accenture.com>
2021-10-12 12:22:55 +0200

    Loader and Notification components created

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import Header from './Header/Header';
 import Menu from './Menu/Menu';
 import Footer from './Footer/Footer';
+import Loader from './Loader/Loader';
+import Notification from './Notification/Notification';
 
 export {
     Header,
+    Loader,
+    Notification,
     Menu,
     Footer
 };
--- src/controllers/App/App.jsx
+++ src/controllers/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Footer } from '../../components';
+import { Header, Footer, Loader, Notification } from '../../components';
 import { Fetch } from '../../services/api'
 
 const FETCH_OPTIONS = {
@@ -15,10 +15,14 @@ class App extends Component {
             <Fetch path={'general'} options={FETCH_OPTIONS}>
                 {({ data, loading, error }) => {
                     if (error) {
-                        return <p>{error.message}</p>;
+                      return (
+                        <Notification type="error"
+                            message= {error.message}
+                        />
+                    );
                     }
                     if (loading) {
-                        return <p>Loading ...</p>;
+                      return <Loader />;
                     }
                     if (data && data.logo) {
                         return <Header logo={data.logo} />
