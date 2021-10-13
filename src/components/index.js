145088fb8c9cd17d402e5a7bf2931432380ec8f7
Martín Vega <maria.dolores.martin@accenture.com>
2021-10-13 12:42:21 +0200

    Info component created

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Menu from './Menu/Menu';
 import Footer from './Footer/Footer';
 import Loader from './Loader/Loader';
 import Notification from './Notification/Notification';
+import Info from './Info/Info';
 
 export {
     Header,
     Loader,
     Notification,
+    Info,
     Menu,
     Footer
 };
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Fetch } from '../../services/api'
+import { Info, Loader, Notification} from '../../components';
+
+const FETCH_OPTIONS = {
+    method: 'GET',
+    headers: {}
+};
+
 
 class Home extends React.Component {
     render() {
         return (
-            <div className="Home">
-                Home Page
-            </div>
+            <div className="Home__info">
+                <Fetch path={'general'} options={FETCH_OPTIONS}>
+                    {({ data, loading, error }) => {
+                        if (error) {
+                            return (
+                                <Notification type="error"
+                                    message= {error.message}
+                                />
+                            );
+                        }
+                        if (loading) {
+                            return <Loader />;
+                        }
+                        if (data) {
+                            return (
+                                <Info title="About Accenture"
+                                data={data}
+                                />
+                            );
+                        }
+                        return <Loader />;
+                    }}
+                    
+                </Fetch>
+        </div>
         );
     }
 }
